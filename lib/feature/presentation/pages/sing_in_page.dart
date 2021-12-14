2538a2fa5b11ec561fb34de8ec902529a3a62d00
Kazi Saidul Islam <kazmdsaidul@gmail.com>
2021-12-14 18:02:54 +0600

    added sing in page

--- lib/feature/presentation/pages/sing_in_page.dart
+++ lib/feature/presentation/pages/sing_in_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _bodyWidet(),
+    );
+  }
+}
